2d3e4e9372eff58c19300f3e99cf48c7d66401be
MaryanneShaw <maryannejshaw@gmail.com>
2026-07-17 19:59:39 +1200

    Initialize Mapbox map with access token and controls

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+mapboxgl.accessToken = 'YOUR_PUBLIC_MAPBOX_TOKEN';
 
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/maryanneshaw/cmrn1q8q5001f01srg3pe85ke',
+  center: [177.5505, -18.1819],
+  zoom: 17
+});
+
+map.addControl(new mapboxgl.NavigationControl());
